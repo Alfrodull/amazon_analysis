198ab8610c70e118df9d8272af8e96ccadf65a09
alfrodull <fanjl92@gmail.com>
2014-04-26 12:39:14 +0800

    添加了获取价格信息的函数

--- datahelper.py
+++ datahelper.py
@@ -2,14 +2,26 @@ import urllib
 import simplejson as json
 
 def get_product_data(asin):
-	'''根据输入的asin获取商品信息'''
+	'''use asin to get get_product_data '''
 	target_url = 'http://112.124.1.3:8004/api/commodity/' + asin
 	return json.loads(urllib.urlopen(target_url).read())
 
 def get_star_list(product_data):
-	'''从获取的数据中提取评分'''
-	star_list = [float(single_review['star'].split()[0]) \
-                 for single_review in product_data['review']]
-    return star_list
+	'''get star infomation from product data'''
+	return [float(single_review['star'].split()[0]) for single_review in product_data['review']]
 
+def get_price_list(product_data):
+	'''get first price infomation from product data'''
+	price_date_li = []
+	for offer in product_data['offer']:
+		if offer['info']:
+			price_date_li.append((offer['info'][0]['price'],offer['info'][0]['timestamp']))
+	return price_date_li
 
+if __name__ == '__main__':
+	# product_data = get_product_data('B00D386JBA')
+	product_data = get_product_data('B003FGWY1O')
+	# sl = get_star_list(product_data)
+	# print sl
+	pdl = get_price_list(product_data)
+	print pdl
